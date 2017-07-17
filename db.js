c6e036e6571f095a92e4925136d265f4e11543eb
逍龙 <junling.xjl@alibaba-inc.com>
2017-07-17 10:05:54 +0800

    rebuild

---
+++ db.js
@@ -0,0 +1,19 @@
+db.pages.insert({ "name" : "jzfp", "text" : "精准扶贫", "url" : "/jzfp", "group" : "mlxc", "tabs" : [ { "name" : "example", "text" : "先进事例", "url" : "" }, { "name" : "dynamic", "text" : "扶贫动态", "url" : "" }, { "name" : "policy", "text" : "扶贫政策", "url" : "" }, { "name" : "vilage", "text" : "一村一品", "url" : "" } ], "banners" : [] })
+
+db.pages.insert({ "name" : "ddjs", "url" : "/ddjs", "group" : "mlxc", "tabs" : [ { "name" : "practice", "text" : "两学一做", "url" : "" }, { "name" : "basic", "text" : "基层建设", "url" : "" } ], "banners" : [ ], "text" : "党的建设" })
+
+db.pages.insert({  "name" : "wybs", "url" : "/wybs", "group" : "bmfw", "tabs" : [ { "name" : "policy", "text" : "政策解读", "url" : "" }, { "name" : "introduction", "text" : "办证指南", "url" : "" } ], "banners" : [ ], "text" : "我要办事", "types" : { "policy" : [ { "name" : "iwnkwlqe", "text" : "义务教育" }, { "name" : "iww26plo", "text" : "医疗保险" }, { "name" : "ix0g7ksw", "text" : "扶贫政策" } ], "introduction" : [ { "name" : "iwm07gri", "text" : "结婚证" }, { "name" : "iwm07grj", "text" : "离婚证" }, { "name" : "iwm07grk", "text" : "低保证明" }, { "name" : "iwm07grl", "text" : "未婚证" } ] } })
+
+db.pages.insert({  "name" : "zxxf", "url" : "/zxxf", "group" : "bmfw", "tabs" : [ { "name" : "rule", "text" : "信访条例", "url" : "" }, { "name" : "law", "text" : "法律援助", "url" : "" }, { "name" : "consult", "text" : "发布信访", "url" : "/fbxf" } ], "banners" : [], "text" : "在线信访" })
+
+db.pages.insert({  "name" : "kjzf", "url" : "/kjzf", "group" : "bmfw", "tabs" : [ { "url" : "", "text" : "农家书屋", "name" : "book" }, { "url" : "", "text" : "视频致富", "name" : "video" }, { "url" : "http://www.cnepaper.com/gnrb/h5/html5/2016-10/26/node_1.htm", "text" : "每日读报", "name" : "paper" } ], "banners" : [], "text" : "科技致富" })
+
+db.pages.insert({  "name" : "home", "text" : "网站首页", "tabs" : [ ], "banners" : [], "desc" : "江西省赣州市人民政府，一直秉承为人民服务的宗旨，坚持政务公开，借助互联网的力量，提高政府工作的透明度，增强广大人民参与政务公开的积极性", "url" : "/", "feeds" : [] })
+
+db.pages.insert({  "name" : "xxzx", "text" : "乡贤在线", "tabs" : [ ], "banners" : [], "url" : "/xxzx"})
+
+db.pages.insert({  "name" : "njzx", "url" : "/njzx", "group" : "bmfw", "tabs" : [ { "name" : "online", "text" : "在线农技", "url" : "" }, { "name" : "plant", "text" : "种植业", "url" : "" }, { "name" : "animal", "text" : "畜牧业", "url" : "" }, { "name" : "disaster", "text" : "病虫害上报", "url" : "/bchsb" } ], "banners" : [ ], "text" : "农技在线" })
+
+db.pages.insert({  "name" : "xxzx", "url" : "/xxzx",  "tabs" : [ ], "banners" : [ ], "text" : "乡贤在线" })
+
+db.admins.insert({"account" : "zmwhzhihuiadmin", "password" : "3df407a8876bcb1597419fad3729bd7f" })
